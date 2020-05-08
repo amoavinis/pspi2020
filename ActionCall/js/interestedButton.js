21f2b65f8c625af4a88589064e74ce0c007d6676
Aggelos Moavinis <amoavinis@gmail.com>
2020-05-08 20:38:57 +0300

    fix

--- ActionCall/js/interestedButton.js
+++
@@ -1,9 +0,0 @@
-var clicks = 0;
-var clicked = 1;
-function UpdateClickCount(){
-    clicks += clicked;
-    clicked *= -1;
-    document.getElementById("interested").innerHTML = clicks;
-    element = document.querySelector('.container'); 
-    element.style.visibility = 'visible'; 
-}
