95ae425853c6b1f6007d62dedf677e74a73b2f9f
GEORGE <rrgeorgio@csd.auth.gr>
2020-06-15 18:19:10 +0300

    Fixed timestamp of post in insert_new_post_to_db

--- insert_new_post_to_db.php
+++ insert_new_post_to_db.php
@@ -5,13 +5,14 @@ session_start();
 
 //$post_datetime = $_POST["evDate"] + $_POST["evTime"];
 
-
+date_default_timezone_set('Europe/Athens');
 $post_date = date('d-m-Y', strtotime($_POST['evDate']));
 $post_time = ' '.$_POST['time_hour'].':'.$_POST['time_min'].":00";
 $post_datetime = date('Y-m-d H:i:s', strtotime($post_date.$post_time));
 $latitude=$_POST['lat'];
 $longitude=$_POST['long'];
-$current_date= "SELECT NOW()";
+$current_date= date('Y-m-d H:i:s');
+echo $current_date;
 $insert_post_sql_query = 
 "INSERT INTO `posts`(`poster_email`, `title`, `details`, `city`, `date_of_event`, `date_posted`,`latitude`,`longitude`) 
 VALUES (\"".htmlentities($_SESSION["email"], ENT_QUOTES)."\",\"".htmlentities($_POST["title"], ENT_QUOTES)."\",\"".$_POST["content"]."\", \"".htmlentities($_POST["cityNames"], ENT_QUOTES)."\", \"".htmlentities($post_datetime, ENT_QUOTES)."\",\"".$current_date."\",\"".htmlentities($latitude,ENT_QUOTES)."\", \"".htmlentities($longitude,ENT_QUOTES)."\")
@@ -29,6 +30,6 @@ echo("
     <script> alert(\"You have successfully created your post!\") </script>
     ");
 
-header("Location: ActionCall_forum.php");
+//header("Location: ActionCall_forum.php");
 
 ?>
