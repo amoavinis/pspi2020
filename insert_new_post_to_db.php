a66e23c624286a92c1a71121353de93908d48d64
GEORGE <rrgeorgio@csd.auth.gr>
2020-06-15 17:46:18 +0300

    Added latitude,longitude to actioncalldb

--- insert_new_post_to_db.php
+++ insert_new_post_to_db.php
@@ -9,9 +9,12 @@ session_start();
 $post_date = date('d-m-Y', strtotime($_POST['evDate']));
 $post_time = ' '.$_POST['time_hour'].':'.$_POST['time_min'].":00";
 $post_datetime = date('Y-m-d H:i:s', strtotime($post_date.$post_time));
+$latitude=$_POST['lat'];
+$longitude=$_POST['long'];
+$current_date= "SELECT NOW()";
 $insert_post_sql_query = 
 "INSERT INTO `posts`(`poster_email`, `title`, `details`, `city`, `date_of_event`, `date_posted`,`latitude`,`longitude`) 
-VALUES (\"".htmlentities($_SESSION["email"], ENT_QUOTES)."\",\"".htmlentities($_POST["title"], ENT_QUOTES)."\",\"".$_POST["content"]."\", \"".htmlentities($_POST["cityNames"], ENT_QUOTES)."\", \"".htmlentities($post_datetime, ENT_QUOTES)."\", \"".htmlentities($latitude,ENT_QUOTES)."\", \"".htmlentities($longitude,ENT_QUOTES)."\", NOW())
+VALUES (\"".htmlentities($_SESSION["email"], ENT_QUOTES)."\",\"".htmlentities($_POST["title"], ENT_QUOTES)."\",\"".$_POST["content"]."\", \"".htmlentities($_POST["cityNames"], ENT_QUOTES)."\", \"".htmlentities($post_datetime, ENT_QUOTES)."\",\"".$current_date."\",\"".htmlentities($latitude,ENT_QUOTES)."\", \"".htmlentities($longitude,ENT_QUOTES)."\")
 "
 ;
 
@@ -26,6 +29,6 @@ echo("
     <script> alert(\"You have successfully created your post!\") </script>
     ");
 
-header("Location: ActionCall_create_post.php");
+header("Location: ActionCall_forum.php");
 
 ?>
