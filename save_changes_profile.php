3889c99312d03719a1b970763c9d2e11fe4f5195
potatoGR <amoavinis@gmail.com>
2020-06-16 21:38:46 +0300

    Revert

--- save_changes_profile.php
+++ save_changes_profile.php
@@ -36,8 +36,6 @@
                 ";
 
                 mysqli_query($con, $update_user_elements);
-                $_SESSION["username"] = $username;
-                $_COOKIE["ActionCallUser"] = $username;
 
                 ?>
                 <script>alert('You have successfully changed your username and/or password!');</script>
