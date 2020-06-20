0fd7e6c1de2c003bc0f2db7245d38f2b62027848
Aggelos Moavinis <amoavinis@gmail.com>
2020-06-20 13:27:53 +0300

    fixed delete

--- delete_event.php
+++ delete_event.php
@@ -4,16 +4,20 @@ require('config.php');
 
 session_start();
 
+$delete_interested_query = "DELETE FROM interested WHERE post_id=\"".$_POST['postId']."\"";
+
 $delete_post_query =
 "DELETE
 FROM posts
 WHERE id = \"".$_POST['postId']."\"
-"
-;
+";
 
 
 // Database connection
 $con = include 'config.php';
+
+//Delete interested
+mysqli_query($con, $delete_interested_query);
 
 // Delete specified post
 mysqli_query($con, $delete_post_query);
